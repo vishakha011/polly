8ee4003ce2122010a635bda337f089defda69af3
vishakha011 <khatadevishakha01@gmail.com>
2021-07-01 10:33:32 +0530

    Added has_secure_password and validations to User model

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'webpacker', '~> 5.0'
 gem 'jbuilder', '~> 2.7'
 gem 'bootsnap', '>= 1.4.4', require: false
 gem 'react-rails'
+gem 'bcrypt', '~> 3.1.13'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_01_040028) do
+ActiveRecord::Schema.define(version: 2021_07_01_045319) do
 
   create_table "polls", force: :cascade do |t|
     t.text "title", null: false
@@ -24,6 +24,9 @@ ActiveRecord::Schema.define(version: 2021_07_01_040028) do
     t.string "last_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "polls", "users", on_delete: :cascade
